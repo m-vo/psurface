36782318a5d5129342a890215778af2162f91493
M. Vondano <m-vo@users.noreply.github.com>
2021-10-23 03:08:21 +0200

    add direct key action + refactor

--- streamdeck/ui.py
+++ streamdeck/ui.py
@@ -2,7 +2,7 @@ import os
 from datetime import datetime, timedelta
 from typing import Tuple
 
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageDraw, ImageFont, ImageOps
 from PIL.ImageDraw import Draw
 from StreamDeck.DeviceManager import DeviceManager
 from StreamDeck.Devices import StreamDeck
@@ -64,12 +64,16 @@ class DeckUI:
         if not all([devices["system"], devices["input"], devices["output"]]):
             raise RuntimeError("Could not find and map all streamdecks.")
 
+        self._settings = GlobalSettings()
+
         self._system_surface = SystemSurface(
-            devices["system"], session, layer_controller
+            devices["system"], self._settings, session, layer_controller
         )
-        self._input_surface = InputSurface(devices["input"], session, layer_controller)
+        self._input_surface = InputSurface(
+            devices["input"], self._settings, session, layer_controller
+        )
         self._output_surface = OutputSurface(
-            devices["output"], session, layer_controller
+            devices["output"], self._settings, session, layer_controller
         )
 
         # Displayed entities
@@ -78,30 +82,60 @@ class DeckUI:
     def init(self) -> None:
         all_surfaces = [self._system_surface, self._input_surface, self._output_surface]
 
-        # setup events
-        def set_brightness(level: int) -> None:
-            for surface in all_surfaces:
-                surface.set_brightness(level)
-
-        self._system_surface.brightness_changed_event.append(set_brightness)
-
         # init surfaces
         for surface in all_surfaces:
             surface.init()
 
+        self._layer_controller.select_default()
+        self._settings.set_default_brightness()
+
+
+class GlobalSettings:
+    def __init__(self):
+        self._brightness: int = 0  # 0-4
+        self._direct_action: bool = False
+
+        self.brightness_changed_event = Event()
+
+    @property
+    def brightness(self) -> int:
+        return self._brightness
+
+    def set_default_brightness(self) -> None:
+        self._brightness = 2
+        self.brightness_changed_event()
+
+    def increase_brightness(self) -> None:
+        self._brightness = (self._brightness + 1) % 5
+        self.brightness_changed_event()
+
+    @property
+    def direct_action(self) -> bool:
+        return self._direct_action
+
+    def toggle_direct_action(self) -> None:
+        self._direct_action = not self._direct_action
+
+    def disable_direct_action(self) -> None:
+        self._direct_action = False
+
 
 class Surface:
     def __init__(
-        self, device: StreamDeck, session: Session, layer_controller: LayerController
+        self,
+        device: StreamDeck,
+        settings: GlobalSettings,
+        session: Session,
+        layer_controller: LayerController,
     ):
         device.reset()
         self._deck: StreamDeck = device
+        self._settings: GlobalSettings = settings
 
         self._session: Session = session
         self._layer_controller: LayerController = layer_controller
         self._fragment_renderer = FragmentRenderer()
         self._scheduler = BackgroundScheduler()
-        self._brightness = 0
 
         self._keys_down: {}
 
@@ -114,8 +148,14 @@ class Surface:
         self._deck.close()
 
     def init(self):
+        self._settings.brightness_changed_event.append(self._update_brightness)
+
         def key_change(_, key: int, state: bool) -> None:
             if state:
+                if self._settings.direct_action:
+                    self._on_key_down_long(key)
+                    return
+
                 self._scheduler.add_job(
                     self._on_key_down_long,
                     DateTrigger(datetime.now(get_localzone()) + timedelta(seconds=1.0)),
@@ -131,17 +171,14 @@ class Surface:
                     #  skip handler if no job is present - it was already executed (_on_key_down_long)
                     return
 
+                if self._settings.direct_action:
+                    return
+
                 self._on_key_up(key)
 
         self._deck.set_key_callback(key_change)
         self._session.channel_update_event.append(self._on_channel_update)
         self._scheduler.start()
-
-        pass
-
-    def set_brightness(self, level: int):
-        self._brightness = level
-        self._update_brightness()
 
     def _on_key_down(self, key: int):
         pass
@@ -162,7 +199,7 @@ class Surface:
         value_set = (streamdeck_original_map, streamdeck_xl_map)[
             isinstance(self._deck, StreamDeckXL)
         ]
-        self._deck.set_brightness(value_set[self._brightness])
+        self._deck.set_brightness(value_set[self._settings.brightness])
 
     def _set_image(self, key: int, image: Image):
         with self._deck:
@@ -358,27 +395,30 @@ class SystemSurface(Surface):
     KEY_BRIGHTNESS = 14
     KEY_INPUTS = 0
     KEY_S_DCA = 5
+    KEY_DIRECT_ACTION = 4
 
     def __init__(
-        self, device: StreamDeck, session: Session, layer_controller: LayerController
+        self,
+        device: StreamDeck,
+        settings: GlobalSettings,
+        session: Session,
+        layer_controller: LayerController,
     ):
-        super(SystemSurface, self).__init__(device, session, layer_controller)
+        super(SystemSurface, self).__init__(device, settings, session, layer_controller)
 
         self._assets["icon_brightness"] = os.path.join(
             self._assets_path, "brightness.png"
         )
-
+        self._assets["icon_direct"] = os.path.join(self._assets_path, "direct.png")
         self._assets["icon_left"] = os.path.join(self._assets_path, "left.png")
-
-        self.brightness_changed_event = Event()
 
     def init(self):
         super(SystemSurface, self).init()
 
         self._layer_controller.selection_update_event.append(self._on_select_change)
-        self._layer_controller.select_default()
 
-        self.brightness_changed_event(2)
+        self._update_brightness()
+        self._update_direct_action()
 
     def _on_select_change(self):
         self._set_image(self.KEY_INPUTS, self._render_inputs_button())
@@ -386,8 +426,12 @@ class SystemSurface(Surface):
 
     def _on_key_down(self, key: int):
         if key == self.KEY_BRIGHTNESS:
-            new_brightness = (self._brightness + 1) % 5
-            self.brightness_changed_event(new_brightness)
+            self._settings.increase_brightness()
+            return
+
+        if key == self.KEY_DIRECT_ACTION:
+            self._settings.toggle_direct_action()
+            self._update_direct_action()
 
     def _on_key_up(self, key: int):
         if key == self.KEY_INPUTS:
@@ -402,10 +446,17 @@ class SystemSurface(Surface):
             self._layer_controller.clear_s_dca()
             return
 
+        # ignore direct action for other system keys
+        self._on_key_down(key)
+        self._on_key_up(key)
+
     def _update_brightness(self):
         super(SystemSurface, self)._update_brightness()
 
         self._set_image(self.KEY_BRIGHTNESS, self._render_brightness_indicator())
+
+    def _update_direct_action(self):
+        self._set_image(self.KEY_DIRECT_ACTION, self._render_direct_action_button())
 
     def _render_brightness_indicator(self) -> Image:
         image = PILHelper.create_scaled_image(
@@ -417,13 +468,35 @@ class SystemSurface(Surface):
         x = image.width - 10
         y_bot = image.height - 12
         y_top = 12
-        y_level = y_bot - 8 - (self._brightness * (y_bot - y_top - 8) / 4)
+        y_level = y_bot - 8 - (self._settings.brightness * (y_bot - y_top - 8) / 4)
         width = 3
 
         draw = ImageDraw.Draw(image)
 
         draw.line((x, y_bot, x, y_top), fill=(50, 50, 50), width=width)
         draw.line((x, y_bot, x, y_level), fill="white", width=width)
+
+        return image
+
+    def _render_direct_action_button(self) -> Image:
+        image = PILHelper.create_scaled_image(
+            self._deck,
+            Image.open(self._assets["icon_direct"]),
+            margins=[16, 18, 18, 15],
+        )
+
+        if self._settings.direct_action:
+            active_color = (255, 0, 50)
+            image = ImageOps.colorize(
+                image.convert("L"), black="black", white=active_color
+            )
+
+            draw = ImageDraw.Draw(image)
+            draw.ellipse(
+                (5, 5, image.width - 10, image.height - 10),
+                outline=active_color,
+                width=4,
+            )
 
         return image
 
